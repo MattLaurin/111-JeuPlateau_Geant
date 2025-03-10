f5f33b2b11242ba46ffd689e7070d0e7c9c9b9e8
WillyFoguep <63255177+WillyFoguep@users.noreply.github.com>
2025-03-10 15:45:49 -0400

    plateau
    création et affichage du plateau

--- jeu/src/plateau.java
+++ jeu/src/plateau.java
@@ -1,18 +1,36 @@
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class plateau {
-    private Map<String,Character> mapPlateau = new LinkedHashMap<>();
-    private String[] lettres={"A","B","C","D","E","F","G","H"};
+    private Map<String,String> mapPlateau = new LinkedHashMap<>();
+    private String[] lettres={"A","B","C","D","E","F","G","H","I"};
 
     public plateau(){
 
+        for(int j=9;j>=1;j--){
+
+            for(int i=0;i< lettres.length;i++){
+                mapPlateau.put(lettres[i]+j,lettres[i]+j);
+            }
+        }
     }
     public void play(){
 
     }
     public void undo(){
 
+    }
+
+    public void printPlateau(){
+        List<String> keys = new ArrayList<String>(mapPlateau.keySet());
+        int i = 1;
+        for(String key : keys){
+            String value = mapPlateau.get(key);
+            System.out.print(value + "   ");
+            if(i % 9 == 0){
+                System.out.println();
+            }
+            i++;
+        }
+        System.out.println("-------------------------");
     }
 }
