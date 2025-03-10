37d944ea85ea01295ebc18e748beade53e0013b0
WillyFoguep <63255177+WillyFoguep@users.noreply.github.com>
2025-03-10 14:25:52 -0400

    quelques  classe de base

---
+++ jeu/src/plateau.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class plateau {
+    private Map<String,Character> mapPlateau = new LinkedHashMap<>();
+    private String[] lettres={"A","B","C","D","E","F","G","H"};
+
+    public plateau(){
+
+    }
+    public void play(){
+
+    }
+    public void undo(){
+
+    }
+}
