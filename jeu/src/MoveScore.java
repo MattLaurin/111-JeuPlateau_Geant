54597bde616398d711ff3c3d395c3d76ae9e03c3
Matthieu Laurin <matthieulaurin@mac.home>
2025-04-01 15:13:28 -0400

    ajout de movescore car petit oubli

---
+++ jeu/src/MoveScore.java
@@ -0,0 +1,11 @@
+// Pour multiThreading
+
+public class MoveScore { 
+    String move;
+    int score; 
+
+    public MoveScore(String move, int score) {
+        this.move = move;
+        this.score = score;
+    }
+}
